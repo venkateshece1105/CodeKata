d90364744a6897b6d4653184c78792263d514145
Venkatesh S <51760002+venkateshece1105@users.noreply.github.com>
2019-07-09 15:25:50 +0530

    Update kata2.c

--- kata2.c
+++ kata2.c
@@ -2,7 +2,9 @@
 int main(){
     int n;
     scanf("%d",&n);
-    while(n>0)
+    if(n<0)
+    printf("invalid");
+    else
     {
     if(n%2==0)
     {
@@ -12,9 +14,10 @@ int main(){
     {
     printf("Odd");
     }
-    return 0;
+    
     }
-    printf("invalid");
+ return 0;   
 }
+
 
 
