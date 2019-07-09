d4e3943fd97c747aa82352d215e7ca399a0fc13d
Venkatesh S <51760002+venkateshece1105@users.noreply.github.com>
2019-07-09 09:49:54 +0530

    Add files via upload

---
+++ kata1.c.c
@@ -0,0 +1,18 @@
+#include<studio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+Printf("enter the number",n);
+if(n>0)
+{
+print("%d is positive",n);
+}
+else if(n>0)
+{
+print(" %negative",n);
+}
+else
+{
+print("neither positive or negative",n);
+}
