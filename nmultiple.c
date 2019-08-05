6d3b4fa29e9c2212d0b4a8a38e5436a98e1ef473
Venkatesh S <51760002+venkateshece1105@users.noreply.github.com>
2019-08-05 21:40:02 +0530

    Add files via upload

---
+++ nmultiple.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+        int sum=0,i;
+        int n=5;
+        scanf("%ld",&n);
+        for(i=1;i<=5;i++)
+        {
+            printf(" \n %ld",n*i);
+        }
+}
