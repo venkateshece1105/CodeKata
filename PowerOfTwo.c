4a2c444a55acdf1c37f24f42435ed6c7bb0b18ef
Venkatesh S <51760002+venkateshece1105@users.noreply.github.com>
2019-08-06 12:43:27 +0530

    Update PowerOfTwo.c

--- PowerOfTwo.c
+++ PowerOfTwo.c
@@ -1,20 +1,12 @@
-#include<stdio.h>
 
+#include <stdio.h>
 int main()
-
 {
- int n,p;
-
- scanf("%d",&n);
- 
-if(n==2^p)
- 
-printf("yes");
-
- else
-
- printf("no");
-
+   int num;
+   scanf("%d", &num);
+   if((num != 1) && ((num &(num - 1)) == 0))
+      printf("yes");
+   else
+      printf("no");
  return 0;
-
 }
