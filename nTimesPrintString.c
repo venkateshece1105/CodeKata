dce464717214c3628f243800e0f52cf9e16efb11
Venkatesh S <51760002+venkateshece1105@users.noreply.github.com>
2019-08-06 14:30:54 +0530

    Add files via upload

---
+++ nTimesPrintString.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    char a[100];
+
+    int n,i;
+ 
+  int count=1;
+ 
+   scanf("%s",&a);
+
+    scanf("%d",&n);
+
+   
+ for(i=1;i<=n;i++)
+ 
+  {
+  
+     count++;
+ 
+       printf("\n %s",a);
+ 
+  }
+   
+ return 0;
+
+}
